bd810e45eba028bbc27b7b6a2c049ad71b2aae0f
sciberbee <yby0907200@gmail.com>
2024-03-02 23:55:45 +0900

    Send Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const App = () => {
-    return <div>App</div>;
-};
+function App() {
+    return (
+        <div>
+            <h1>안녕하세요!</h1>
+            <p>저는 방승현입니다.</p>
+            <a href="https://github.com/sciberbee" target="_blank">
+                <button>GitHub</button>
+            </a>
+        </div>
+    );
+}
 
 export default App;
